734de76ee5b21d29e9510405345e984f1c66b786
Jonathan Gunnewegh <jongun92@outlook.com>
2017-12-05 15:24:14 -0800

    Changed RPN to header and RPNdefined to Definitions

--- Poly_c/Poly_c/Main.cpp
+++ Poly_c/Poly_c/Main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-#include "RPN.h"
-#include "RPNdefined.h"
+#include "Header.h"
+#include "Definitions.h"
 #include <iostream>
 #include <stack>
 #include "Polynomial.h"
